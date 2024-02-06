19f75e12d54fefada7a01b683ffc31fabde42826
Dynse Clyde Sacote <beetlemon.cwts@gmail.com>
2024-02-06 22:16:08 +0800

    Imported other RESTful features in Flask

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # app.py
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, g, request, abort, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists
 from sqlalchemy import func, exc, create_engine, select
 from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'Dolph Laserhawk and Alex Taylor are a complicated couple'
 connection_str = f'postgresql://postgres.xgmzxcdyetduyrypqbhh:RaymanIsAwesome123@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 db = create_engine(connection_str)
 
