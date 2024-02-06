f3aa40ed08848377215222936d5d0250fe076a85
Dynse Clyde Sacote <beetlemon.cwts@gmail.com>
2024-02-06 19:58:50 +0800

    Old formula returned 1 on empty table, trying another

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ session = Session()
 @app.route('/admin/start', methods=['GET'])
 def create_first_admin():
     # this returns int
-    user_count = session.query(User).with_entities(func.count()).scalar()
+    user_count = session.query(User).count()
+    
     return str(user_count)
 
 @app.route('/admin/login', methods=['GET'])
