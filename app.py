25efccd0f1a56e7fb4d5fb1ba35653ca1ffaccf6
Dynse Clyde Sacote <beetlemon.cwts@gmail.com>
2024-02-06 13:50:19 +0800

    Tested query if user table is empty or not

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists
-from sqlalchemy import func, exc, create_engine
+from sqlalchemy import func, exc, create_engine, select
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
@@ -12,11 +13,16 @@ db = create_engine(connection_str)
 from models import *
 
 Project.metadata.create_all(db)
-Users.metadata.create_all(db)
+User.metadata.create_all(db)
+
+Session = sessionmaker(bind=db)
+session = Session()
 
 @app.route('/admin/start', methods=['GET'])
 def create_first_admin():
-    return "start admin"
+    stmt = select(User)
+    result = session.execute(stmt)
+    return result
 
 @app.route('/admin/login', methods=['GET'])
 def admin_login():
