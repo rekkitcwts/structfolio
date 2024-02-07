289652b89e6e0769240fe13f57d5fa761acd8f01
Dynse Clyde Sacote <clydedatastruct@gmx.com>
2024-02-07 20:14:57 +0800

    Added verify password method

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists
 from sqlalchemy import func, exc, create_engine, select
 from sqlalchemy.orm import sessionmaker
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Dolph Laserhawk and Alex Taylor are a complicated couple'
 connection_str = f'postgresql://postgres.xgmzxcdyetduyrypqbhh:RaymanIsAwesome123@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 db = create_engine(connection_str)
+auth = HTTPBasicAuth()
 
 from models import *
 
@@ -33,8 +35,27 @@ def create_first_admin():
         session.commit()
         return "Create default admin creds here"
 
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+@app.route('/api/admin/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
 @app.route('/api/admin/login', methods=['POST'])
 def api_admin_login():
+    
     return jsonify({ 'hoie': "JSON with token"})
 
 @app.route('/admin/login', methods=['GET'])
